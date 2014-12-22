15191fda533825dd090f07528e3e8620f520990b
Phil Nguyen <pnguyen06@gmail.com>
2014-12-22 13:03:55 -0600

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Supertheque::Application.config.secret_key_base = '74b1983c7c7821be1d0574f773fc71395f65610ab90500602936020445c3a83096b999754fda9150c85ec9ae7d58c7272896db38b93d79314e029c0809c52287'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Supertheque::Application.config.session_store :cookie_store, key: '_supertheque_session'
