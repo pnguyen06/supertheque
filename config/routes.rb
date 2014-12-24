748574f90467a9071d8c192e600364f6e08812fe
Phil Nguyen <pnguyen06@gmail.com>
2014-12-23 21:37:15 -0600

    Monster game

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Supertheque::Application.routes.draw do
+  root "pages#home"
+  get "about" => "pages#about"
+
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
